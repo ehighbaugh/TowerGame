602d843a2add7269aef621f88238e796d43c81e3
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-04-02 19:57:02 -0400

    created class files for game, invader, map, path, tower; deleted program.cs (main dupe)

---
+++ tower-game/tower-game/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tower_game 
+{
+    class Game
+    {
+        public static void Main()
+        {
+            Map map = new Map();
+
+            map.Width = 8;
+            map.Height = 5;
+
+            int area = map.Width * map.Height;
+        }
+    }
+}
---
+++ tower-game/tower-game/Map.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tower_game
+{
+    class Map
+    {
+        public int Width;
+        public int Height;
+    }
+}
