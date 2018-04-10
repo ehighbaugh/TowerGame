2c458ec89989370c27765f153cb371a7e6104be2
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-04-09 23:01:48 -0400

    completed level.cs

--- tower-game/tower-game/Level.cs
+++ tower-game/tower-game/Level.cs
@@ -4,18 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//Returns: true if the player wins, false if they lose
-public bool Play()
+namespace tower_game
 {
-    //Run until all invaders killed or invader reaches end
-    int remainingInvaders = _invaders.Length;
-
-    while(remainingInvaders > 0)
+    class Level
     {
-        //Each tower has opp to fire on invaders
-        foreach(Tower tower in towers)
+        private readonly Invader[] _invaders;
+
+        public Tower[] Towers { get; set; }
+
+        public Level(Invader[] invaders)
         {
-            tower.FireOnIvaders(_invaders);
+            _invaders = invaders;
+        }
+
+        //Returns: true if the player wins, false if they lose
+        public bool Play()
+        {
+            //Run until all invaders killed or invader reaches end
+            int remainingInvaders = _invaders.Length;
+
+            while (remainingInvaders > 0)
+            {
+                //Each tower has opp to fire on invaders
+                foreach (Tower tower in Towers)
+                {
+                    tower.FireOnIvaders(_invaders);
+                }
+
+                //Count and move active invaders
+                remainingInvaders = 0;
+                foreach (Invader invader in _invaders)
+                {
+                    if (invader.IsActive)
+                    {
+                        invader.Move();
+
+                        if (invader.HasScored)
+                        {
+                            return false;
+                        }
+
+                        remainingInvaders++;
+                    }
+                }
+            }
+
+            return true;
         }
     }
 }
